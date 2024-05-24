953be377b54059bf322078c4a56e54c104f010c8
seung365 <bdh3659@naver.com>
2024-05-24 16:08:03 +0900

    feat: Board, Square 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+import Board from "./components/Board";
+import './App.css';
 
 function App() {
   return (
     <div >
-      
+      <div className="game">
+        <div className="game-board">
+          <Board/>
+        </div>
+        <div className="game-info">
+          game-info
+        </div>
+      </div>
     </div>
   );
 }
