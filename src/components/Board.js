15bac5ad303242f77c47c6150aa567cc11d70033
seung365 <bdh3659@naver.com>
2024-05-24 20:54:15 +0900

    feat: Square 클릭시 O,X 출력

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,33 +1,50 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Square from "./Square";
 import "./Board.css"
 
 
-export default class Board extends Component {
-  renderSquare(i) {
-    return <Square/>
-  }
+  const Board = () => {
+    
+    const[squares,setSquares] = useState(Array(9).fill(null))           // 변수이름 getter와  state를 정하는 함수 setter
+    const [xIsNext , setXIsNext] = useState(true)
 
-  render() {
+    const status = `Next player ${xIsNext ? 'X' : 'O'}`
+    const handleClick = (i) => {
+      const newSquares = squares.slice()
+      newSquares[i] = xIsNext ? 'X' : 'O'
+      setSquares(newSquares)
+      setXIsNext(current => !current)
+    }
+
+    const renderSquare = (i) => {
+      return (
+            <Square value={squares[i]}
+            onClick={() => handleClick(i)}
+            />
+            )
+    }
+  
     return (
       <div>
-        <div className="status">Next Player: X, O</div>
+        <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
         </div>
       </div>
     );
+  
   }
-}
+  export default Board
+
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React from "react"
 import "./Square.css"
 
-export default class Square extends React.Component {
-  render() {
-    return <button className="square">
-      
-    </button>;
-  }
+const Square = (props) => { // 구조분해 할당으로도 가능하다.. {onClick, value} 해서 그냥 onClick이랑 value도 바로 사용 가능
+  
+    return (
+      <button className="square" 
+      onClick={() => {props.onClick()}}>
+      {props.value}
+      </button>
+    )
 }
+export default Square
+
