3903c6239705823bc32459dc1c0ee86d0fd3abda
seung365 <bdh3659@naver.com>
2024-05-25 20:37:25 +0900

    feat: step에 따른 Jumpto 구현

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css'
 function App() {
   const [history, sethistory] = useState([{squares:Array(9).fill(null)}])
   const [xIsNext , setXIsNext] = useState(true)
+  const [stepNumber, setStepNumber] = useState(0)
+  const current = history[stepNumber]
 
   const calculateWinner = (squares) => {
     const lines = [
@@ -26,8 +28,8 @@ function App() {
       }
     } return null
   }
-  const current = history[history.length -1]
   const winner = calculateWinner(current.squares)
+
   let status
   if(winner) {
     status = `Winner: ${winner}`
@@ -36,15 +38,33 @@ function App() {
   }
 
   const handleClick = (i) => {
-    const newSquares = current.squares.slice()
+    const newHistory = history.slice(0, stepNumber+1)
+    const newCurrent = newHistory[newHistory.length-1]
+    const newSquares = newCurrent.squares.slice()
     if(calculateWinner(newSquares) || newSquares[i]) {
       return
     }
     newSquares[i] = xIsNext ? 'X' : 'O' 
-    sethistory([...history, { squares: newSquares }])
-    setXIsNext(prev => !prev)
+    sethistory([...newHistory, { squares: newSquares }])
+    setXIsNext(current => !current)
+    setStepNumber(newHistory.length)
   }
 
+  const moves = history.map((step,move) => { // 첫번째 현재 요소 두번째 인덱스 세번째 원본 배열
+    const desc = move ?
+    'Go to move #' + move :
+    'Go to game start'
+    return(
+      <li key={move}> 
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    )
+  })
+
+  const jumpTo = (step) => {
+    setStepNumber(step)
+    setXIsNext(step % 2 === 0)
+  }
 
 
   return (
@@ -56,9 +76,8 @@ function App() {
             />
         </div>
         <div className="game-info">
-        <div className="status">{status}</div>
-
-          game-info
+          <div className="status">{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     </div>
